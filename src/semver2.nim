7bfb335ff4403b62d5db8ff1e9b895b26a0c49e2
Zack Guard <zacharyguard@gmail.com>
2021-12-08 11:20:39 +0800

    Add GPL 3.0 license text

--- src/semver2.nim
+++ src/semver2.nim
@@ -1,3 +1,20 @@
+# Copyright (C) 2021 Zack Guard
+# 
+# This file is part of semver2.
+# 
+# semver2 is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# semver2 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with semver2.  If not, see <http://www.gnu.org/licenses/>.
+
 import ./semver2/types
 import ./semver2/parse
 import ./semver2/compare
